65477296609f8b46f17a8de2b4468e90678801be
Rosemary Liu <rliu18@gmail.com>
2022-07-11 13:07:42 -0700

    practice union types

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,17 @@
 "use strict";
-function calculateTax(income) {
-    if (income < 50000)
-        return income * 1.2;
-    return 1.3;
+let employee = {
+    id: 1,
+    name: "",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
 }
+kgToLbs(10);
+kgToLbs("10kg");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,12 @@ let employee: Employee = {
     console.log(date);
   },
 };
+
+function kgToLbs(weight: number | string): number {
+  //narrowing
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
+
+kgToLbs(10);
+kgToLbs("10kg");
